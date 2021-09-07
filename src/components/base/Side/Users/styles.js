7062a96e0763918e9dd0978eba93bd5101b2a64d
Vasil Kiselevich <kvg681982@gmail.com>
2021-09-08 02:14:35 +0300

    HW2_v2

--- src/components/base/Side/Users/styles.js
+++ src/components/base/Side/Users/styles.js
@@ -1,10 +1,19 @@
 export default {
   users: {
     display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    flexGrow: 5,
+    flexDirection: 'row ',
+    // justifyContent: 'space-around',
+    flexGrow: 2,
     padding: '15px',
     backgroundColor: '#39ccdf',
+  },
+  userItem: {
+    display: 'flex',
+    flexDirection: 'column ',
+    padding: '5px',
+    paddingRight: '25px',
+    color: '#fff',
+    textDecoration: 'none',
+    fontWeight: '600',
   },
 };
